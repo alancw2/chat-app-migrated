35ae7360c2a1a788ad22df60af01fdad1c5a239b
alan <nala.c.ward@gmail.com>
2026-03-20 17:42:58 +0000

    fixed merge conflicts and version issues

--- include/Config.hpp
+++ include/Config.hpp
@@ -1,7 +1,12 @@
 #ifndef CONFIG_HPP
 #define CONFIG_HPP
+
 #include <regex>
 
 namespace Config {
-	static constexpr std:string_view APP_NICKNAME_PATTERN = R"(^[^\s](?:[a-zA-Z0-9 ._,@#\-]*[^\s])?$)";
+    inline const std::regex APP_NICKNAME_PATTERN(
+        R"(^[^\s](?:[a-zA-Z0-9 ._,@#\-]*[^\s])?$)"
+    );
 }
+
+#endif
--- src/Commands/Commands.cpp
+++ src/Commands/Commands.cpp
@@ -1,7 +1,9 @@
 #include "../../include/Commands.hpp"
 #include "../../include/ConnectionHandler.hpp"
+#include "../../include/Config.hpp"
 #include <iostream>
 #include <format>
+#include <regex>
 
 void Commands::joinNewRoom(std::shared_ptr<ConnectionHandler> conn, std::string& newRoom) {
     newRoom.erase(std::remove(newRoom.begin(), newRoom.end(), '\n'), newRoom.end());
@@ -13,20 +15,13 @@ void Commands::joinNewRoom(std::shared_ptr<ConnectionHandler> conn, std::string&
     }
 }
 void Commands::setNewNickname(std::shared_ptr<ConnectionHandler> conn, std::string& newNick) {
-    newNick.erase(std::remove(newNick.begin(), newNick.end(), '\n'), newNick.end());
-    newNick.erase(std::remove(newNick.begin(), newNick.end(), '\r'), newNick.end());
-    newNick.erase(newNick.find_last_not_of(" ") + 1);
-
-    if (newNick != "") {
-        std::cerr << "0 - nickname change success" << std::endl;
+    if (std::regex_match(newNick, Config::APP_NICKNAME_PATTERN)) {
         conn->setClientLabel(newNick);
-        return;
-    } 
-    std::cerr << "1 - nickname change fail (bad nickname)" << std::endl;
-    return;
-    
-
-} 
+    } else {
+        std::cerr << "1 - nickname change fail (bad nickname)" << std::endl;
+        
+    }
+}
 std::string Commands::getUsersInRoom(std::shared_ptr<ConnectionHandler> conn, std::vector<std::shared_ptr<ConnectionHandler>>& connections) {
     std::string users = "";
     for (const auto& connection : connections) {
